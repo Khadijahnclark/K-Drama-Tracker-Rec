1452d36feae0e8fb0fa009fc83385867e9e3859e
Khadijah Clark <Khadijahnclark@gmail.com>
2025-07-14 14:44:25 -0500

    installing react

--- backend/server/index.js
+++ backend/server/index.js
@@ -6,10 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/api", (req, res) => {
+    res.json({message: "Hellow from server!"});
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get("/api", (req, res) => {
-    res.json({message: "Hellow from server!"});
-});
